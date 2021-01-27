6b9abd8dc4cfbd230697425095341bb80d0cb9eb
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-01-27 11:35:19 -0600

    feat: Made basic hero specs

--- src/ConsoleAdventureGame.java
+++ src/ConsoleAdventureGame.java
@@ -5,18 +5,20 @@ public class ConsoleAdventureGame {
         Scanner scanner = new Scanner(System.in);
 
         String name = "Player";
+        Hero ourHero = new Hero();
 
         System.out.println("Would you lke to play?");
 
         String toPlay = scanner.nextLine();
 
-        if(toPlay.equals("yes")){
+        if(toPlay.equalsIgnoreCase("yes")){
             System.out.println("What is your name?");
             name = scanner.nextLine();
 
-            System.out.println(name);
+            ourHero.name = name;
+            System.out.println("Hello " + ourHero.name +  "\nHealth is: " + ourHero.health);
         } else {
-            System.out.println("Come back when you are");
+            System.out.println("Come back when you are ready.");
         }
 
 
---
+++ src/Hero.java
@@ -0,0 +1,6 @@
+public class Hero {
+    int health = 100;
+    int strength = 20;
+    int defense = 15;
+    String name = "";
+}
