e4a0ff9936c4fbb5034965078f5060a52bf9870e
Chris Agold <christophersagold@gmail.com>
2021-01-27 11:56:09 -0600

    added user choices

--- src/ConsoleAdventureGame.java
+++ src/ConsoleAdventureGame.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 
+
 public class ConsoleAdventureGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Variables
         String name = "Player";
         Hero ourHero = new Hero();
         goblin goblinOne = new goblin();
@@ -12,6 +14,7 @@ public class ConsoleAdventureGame {
 
         String toPlay = scanner.nextLine();
 
+        // Initialize Game
         if(toPlay.equalsIgnoreCase("yes")){
             System.out.println("What is your name?");
             name = scanner.nextLine();
@@ -27,15 +30,37 @@ public class ConsoleAdventureGame {
 
     }
 
+    // User Choice
     public static void turn() {
         Scanner scanner = new Scanner(System.in);
+        // Choices for User
         System.out.println("What do you do?");
-        String action = scanner.nextLine();
+        System.out.println("1 Attack");
+        System.out.println("2 Defend");
+        System.out.println("3 Drink Potion");
+        System.out.println("4 Run Away");
+        int action = scanner.nextInt();
 
-        if(action.equalsIgnoreCase("run")){
-            System.out.println("You ran away");
+        // Attack
+        if(action == 1) {
+            System.out.println("Attack!");
         }
+        // Defend
+        else if(action == 2) {
+            System.out.println("Defend");
+        }
+        // Drink Potion
+        else if(action == 3) {
+            System.out.println("Drink up");
+        }
+        // Escape
+        else if(action == 4) {
+            System.out.println("You ran away....");
+        }
+        else {
+            System.out.println("Chose a number!");
+            turn();
+        }
+
     }
-
-
 }
--- src/Hero.java
+++ src/Hero.java
@@ -2,5 +2,7 @@ public class Hero {
     int health = 100;
     int strength = 20;
     int defense = 15;
+    int healthPotion = 5;
+    int healAmount = 30;
     String name = "";
 }
