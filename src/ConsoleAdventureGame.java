e97f99d98f4b9d11584172ae8f44338f6eeffbb8
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-01-27 11:27:55 -0600

    feat: First questions

--- src/ConsoleAdventureGame.java
+++ src/ConsoleAdventureGame.java
@@ -1,8 +1,26 @@
-public class ConsoleAdventureGame {
+import java.util.Scanner;
 
+public class ConsoleAdventureGame {
     public static void main(String[] args) {
-//        sdgg
-//        asdf
+        Scanner scanner = new Scanner(System.in);
+
+        String name = "Player";
+
+        System.out.println("Would you lke to play?");
+
+        String toPlay = scanner.nextLine();
+
+        if(toPlay.equals("yes")){
+            System.out.println("What is your name?");
+            name = scanner.nextLine();
+
+            System.out.println(name);
+        } else {
+            System.out.println("Come back when you are");
+        }
+
+
+
     }
 
 
