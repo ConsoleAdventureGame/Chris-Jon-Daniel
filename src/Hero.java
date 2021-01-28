47bf15d9da873baad05189b304e77756f705e7c9
Chris Agold <christophersagold@gmail.com>
2021-01-28 09:17:51 -0600

    imported features

--- src/Hero.java
+++ src/Hero.java
@@ -1,8 +1,8 @@
-public class Hero {
-    int health = 100;
-    int strength = 20;
-    int defense = 15;
-    int healthPotion = 5;
-    int healAmount = 30;
-    String name = "";
-}
+//public class Hero {
+//    int health = 100;
+//    int strength = 20;
+//    int defense = 15;
+//    int healthPotion = 5;
+//    int healAmount = 30;
+//    String name = "";
+//}
--- src/goblin.java
+++ src/goblin.java
@@ -1,5 +1,5 @@
-public class goblin {
-    int  enemyHealth = 25;
-    int strength = 5;
-    int defense = 7;
-}
+//public class goblin {
+//    int  enemyHealth = 25;
+//    int strength = 5;
+//    int defense = 7;
+//}
